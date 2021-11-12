ae1269db161974269987d5c5b8c19cc318137693
Nolan D'Souza <n8dsouza@uwaterloo.ca>
2021-11-11 20:43:37 -0500

    Added final dataset + data processing scripts

---
+++ league-ultimate-team/processing/createCards.py
@@ -0,0 +1,103 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+def createCards(df, driver, start=0, end=165):
+
+    print(df)
+
+    # Webpage elements
+    cardListButton = driver.find_element(By.ID, 'card_selector_btn')
+    downloadButton = driver.find_element(By.XPATH, '//*[@id="createCard"]/div[1]/div/div/button[1]')
+    bronzeCard = driver.find_element(By.XPATH, '//*[@id="card_selector_modal_22"]/div/div/div[2]/div/div[4]/a')
+    silverCard = driver.find_element(By.XPATH, '//*[@id="card_selector_modal_22"]/div/div/div[2]/div/div[5]/a')
+    goldCard = driver.find_element(By.XPATH, '//*[@id="card_selector_modal_22"]/div/div/div[2]/div/div[6]/a')
+    legendCard = driver.find_element(By.XPATH, '//*[@id="card_selector_modal_22"]/div/div/div[2]/div/div[17]/a')
+    name = driver.find_element(By.ID, 'form-card-name')
+    overall = driver.find_element(By.ID, 'form-card-rating')
+    position = driver.find_element(By.ID, 'form-card-position')
+    headshot = driver.find_element(By.ID, 'form-card-custom-image')
+    team = driver.find_element(By.ID, 'form-card-club-text')
+    nation = driver.find_element(By.ID, 'form-card-nation-text')
+
+    for i in range(start, end):
+
+        # Change card type
+        cardListButton.click()
+        driver.implicitly_wait(10)
+        cardType = df['Card Type'][i]
+        if cardType == 'Bronze':
+            bronzeCard.click()
+        elif cardType == "Silver":
+            silverCard.click()
+        elif cardType == "Gold":
+            goldCard.click()
+        elif cardType == "Legend":
+            legendCard.click()
+        else:
+            print("ERROR")
+
+        # Change card text
+        name.clear()
+        name.send_keys(df['Player'][i])
+        overall.clear()
+        overall.send_keys(str(int(df['Overall'][i])))
+        position.clear()
+        position.send_keys(df['Position'][i])
+
+        stats = [df['Laning'][i], df['Carry'][i], df['Vision'][i], df['Versatility'][i], df['Threat'][i], df['Experience'][i]]
+
+        for j in range(len(stats)):
+            textBox = driver.find_element(By.ID, 'form-card-attr' + str(j+1))
+            textBox.clear()
+            textBox.send_keys(str(int(stats[j])))
+
+        # Change images
+        headshot.clear()
+        headshot.send_keys(df['Headshot Link'][i])
+        team.clear()
+        team.send_keys(df['Team Logo Link'][i])
+        nation.clear()
+        nation.send_keys(df['Flag Link'][i])
+
+        # Does the card look good enough?
+        while(True):
+            closeDriver = input("Go to next card (Y/N): ")
+            if closeDriver == "Y":
+                downloadButton.click()
+                print("Downloaded", df['Player'][i] + ".png")
+                break
+
+
+if __name__ == "__main__":
+    # Initialization
+    serv = Service(r'C:\Users\Nolan\Desktop\Miscellaneous\Setup\chromedriver_win32\chromedriver.exe')
+    opt = webdriver.ChromeOptions()
+    opt.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(service=serv, options=opt)
+    link = 'https://www.fifarosters.com/create-card'
+    cardStats = pd.read_csv(r'C:\Users\Nolan\Desktop\Liquid Hacks Workspace\Main Dataset.csv')
+
+    # Set up website
+    driver.get(link)
+    expandOptions = driver.find_element(By.XPATH, '//*[@id="createCardForm"]/div[3]/div[1]/h4/a')
+    expandOptions.click()
+    driver.implicitly_wait(10)
+    customImageButton = driver.find_element(By.XPATH, '//*[@id="form-image-type-custom"]')
+    customImageButton.click()
+    driver.implicitly_wait(10)
+    blankBottomButton = driver.find_element(By.XPATH, '//*[@id="form-card-bottom-blank"]')
+    blankBottomButton.click()
+
+    # Change stat titles
+    statTitles = ['LAN', 'CAR', 'VIS', 'VER', 'THR', 'EXP']
+    for i in range(len(statTitles)):
+        textBox = driver.find_element(By.ID, 'form-card-attr' + str(i+1) + '-text')
+        driver.implicitly_wait(10)
+        textBox.clear()
+        textBox.send_keys(statTitles[i])
+
+    # Create cards
+    createCards(cardStats, driver, 0, 5)
+    driver.quit()
---
+++ league-ultimate-team/processing/getChamps.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import csv
+
+def getChampionPositions(region):
+    top, jg, mid, adc, sup = [], [], [], [], []
+    for i in range(len(region)):
+        if region['Pos'][i] == 'Top':
+            top.append(region['Champion'][i])
+        elif region['Pos'][i] == 'Jungle':
+            jg.append(region['Champion'][i])
+        elif region['Pos'][i] == 'Middle':
+            mid.append(region['Champion'][i])
+        elif region['Pos'][i] == 'ADC':
+            adc.append(region['Champion'][i])
+        elif region['Pos'][i] == 'Support':
+            sup.append(region['Champion'][i])
+    table = [top, jg, mid, adc, sup]
+    return table
+
+
+def printDict(d):
+    for key in d:
+        print(key + ":", d[key])
+
+
+def getThreat(df, champDict):
+    threatDict = {}
+    for i in range(len(df)):
+        player = df['player'][i]
+        pool = champDict[df['leaguePosition'][i]]
+
+        if player not in threatDict:
+            threatDict[player] = {}
+            threatDict[player]["gamesPlayed"] = 1
+            threatDict[player]["bannedAgainst"] = 0
+        else:
+            threatDict[player]["gamesPlayed"] += 1
+        
+        for j in range(1, 5):
+            bannedChamp = df['ban' + str(j)][i]
+            if bannedChamp in pool:
+                threatDict[player]["bannedAgainst"] += 1
+    return threatDict
+
+
+if __name__ == "__main__":
+    # Get champs
+    leagues = ['lck', 'lec', 'lcs']
+    positions = ['TOP', 'JG', 'MID', 'ADC', 'SUP']
+    champDict = {}
+    for league in leagues:
+        path = 'C:\\Users\\Nolan\\Desktop\\Liquid Hacks Workspace\\Champion Data\\' + league + 'Champs.csv'
+        champions = pd.read_csv(path)
+        table = getChampionPositions(champions)
+        for i in range(len(positions)):
+            champDict[league + positions[i]] = table[i]
+    
+    # Get threat
+    threatDataset = pd.read_csv(r'C:\Users\Nolan\Desktop\Liquid Hacks Workspace\threatDataset.csv')
+    threatScores = getThreat(threatDataset, champDict)
+    printDict(threatScores)
+
+    # Write threat to CSV
+    output = []
+    for player in threatScores:
+        games = threatScores[player]["gamesPlayed"]
+        banned = threatScores[player]["bannedAgainst"]
+        entry = [player, games, banned]
+        output.append(entry)
+    with open('playerThreats.csv', 'w', newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        header = ['Player', 'gamesPlayed', 'bannedAgainst', 'rawBanScore']
+        writer.writerow(header)
+        writer.writerows(output)
+    
